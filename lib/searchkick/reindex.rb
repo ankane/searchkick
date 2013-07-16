88b0957cc6ff2f5d5c6bb1e24a28f8ced086ff63
Andrew Kane <acekane1@gmail.com>
2013-07-16 03:02:30 -0700

    Less output

--- lib/searchkick/reindex.rb
+++ lib/searchkick/reindex.rb
@@ -15,7 +15,6 @@ module Searchkick
       end
 
       if a = Tire::Alias.find(alias_name)
-        puts "[IMPORT] Alias found: #{Tire::Alias.find(alias_name).indices.to_ary.join(",")}"
         old_indices = Tire::Alias.find(alias_name).indices
         old_indices.each do |index|
           a.indices.delete index
@@ -25,18 +24,15 @@ module Searchkick
         a.save
 
         old_indices.each do |index|
-          puts "[IMPORT] Deleting index: #{index}"
           i = Tire::Index.new(index)
           i.delete if i.exists?
         end
       else
-        puts "[IMPORT] No alias found. Deleting index, creating new one, and setting up alias"
         i = Tire::Index.new(alias_name)
         i.delete if i.exists?
         Tire::Alias.create(name: alias_name, indices: [new_index])
       end
 
-      puts "[IMPORT] Saved alias #{alias_name} pointing to #{new_index}"
       true
     end
 
