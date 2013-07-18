fc7159adbcfea3ade46e8c50a2f619abe2b30ad4
Andrew Kane <acekane1@gmail.com>
2013-07-17 21:23:26 -0700

    Fixed rake task

--- lib/searchkick/tasks.rb
+++ lib/searchkick/tasks.rb
@@ -4,6 +4,6 @@ namespace :searchkick do
   desc "re-index elasticsearch"
   task :reindex => :environment do
     klass = ENV["CLASS"].constantize
-    klass.tire.reindex
+    klass.reindex
   end
 end
