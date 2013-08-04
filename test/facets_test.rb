9fb6ba9bc7900c873d573539c9a7b691a102184b
Andrew Kane <acekane1@gmail.com>
2013-08-03 19:12:13 -0700

    Updated facet tests

--- test/facets_test.rb
+++ test/facets_test.rb
@@ -2,15 +2,21 @@ require_relative "test_helper"
 
 class TestFacets < Minitest::Unit::TestCase
 
-  def test_facets
+  def setup
+    super
     store [
       {name: "Product Show", store_id: 1, in_stock: true, color: "blue"},
       {name: "Product Hide", store_id: 2, in_stock: false, color: "green"},
       {name: "Product B", store_id: 2, in_stock: false, color: "red"}
     ]
+  end
+
+  def test_basic
     assert_equal 2, Product.search("Product", facets: [:store_id]).facets["store_id"]["terms"].size
+  end
+
+  def test_where
     assert_equal 1, Product.search("Product", facets: {store_id: {where: {in_stock: true}}}).facets["store_id"]["terms"].size
-    assert_equal 1, Product.search("Product", facets: {store_id: {where: {in_stock: true, color: "blue"}}}).facets["store_id"]["terms"].size
   end
 
 end
