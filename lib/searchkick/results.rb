5a1ab8b37f49853c1578f759441cd0afb5b3abe7
Andrew Kane <acekane1@gmail.com>
2013-08-06 22:17:16 -0700

    Removed old comments

--- lib/searchkick/results.rb
+++ lib/searchkick/results.rb
@@ -1,8 +1,6 @@
 module Searchkick
   class Results < Tire::Results::Collection
 
-    # TODO use all fields
-    # return nil suggestion if term does not change
     def suggestions
       if @response["suggest"]
         @response["suggest"].values.flat_map{|v| v.first["options"] }.sort_by{|o| -o["score"] }.map{|o| o["text"] }.uniq
