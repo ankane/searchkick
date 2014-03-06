30406b7004d437427a8e71ad142a1b34414dd5f9
Andrew Kane <acekane1@gmail.com>
2014-03-05 22:59:31 -0800

    DRY code

--- lib/searchkick/index.rb
+++ lib/searchkick/index.rb
@@ -7,23 +7,23 @@ module Searchkick
     end
 
     def create(options = {})
-      Searchkick.client.indices.create index: name, body: options
+      client.indices.create index: name, body: options
     end
 
     def delete
-      Searchkick.client.indices.delete index: name
+      client.indices.delete index: name
     end
 
     def exists?
-      Searchkick.client.indices.exists index: name
+      client.indices.exists index: name
     end
 
     def refresh
-      Searchkick.client.indices.refresh index: name
+      client.indices.refresh index: name
     end
 
     def store(record)
-      Searchkick.client.index(
+      client.index(
         index: name,
         type: record.document_type,
         id: record.id,
@@ -32,7 +32,7 @@ module Searchkick
     end
 
     def remove(record)
-      Searchkick.client.delete(
+      client.delete(
         index: name,
         type: record.document_type,
         id: record.id
@@ -41,7 +41,7 @@ module Searchkick
 
     def import(records)
       if records.any?
-        Searchkick.client.bulk(
+        client.bulk(
           index: name,
           type: records.first.document_type,
           body: records.map{|r| data = r.as_indexed_json; {index: {_id: data["_id"] || data["id"] || r.id, data: data}} }
@@ -50,11 +50,18 @@ module Searchkick
     end
 
     def retrieve(document_type, id)
-      Searchkick.client.get_source(
+      client.get_source(
         index: name,
         type: document_type,
         id: id
       )
     end
+
+    protected
+
+    def client
+      Searchkick.client
+    end
+
   end
 end
