2a43d66092b85b573e472b5b45f29c1224660e9d
Andrew Kane <acekane1@gmail.com>
2014-03-07 02:31:58 -0800

    Added previous page and next page

--- lib/searchkick/results.rb
+++ lib/searchkick/results.rb
@@ -84,6 +84,14 @@ module Searchkick
       current_page * per_page
     end
 
+    def previous_page
+      current_page > 1 ? (current_page - 1) : nil
+    end
+
+    def next_page
+      current_page < total_pages ? (current_page + 1) : nil
+    end
+
     protected
 
     def hits
