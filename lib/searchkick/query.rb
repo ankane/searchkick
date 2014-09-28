c474319b0256c6cc7e9bcdf72a62a7562a7e894c
Sébastien Fieloux <sebfie@yahoo.fr>
2014-09-28 12:11:00 +0200

    Add the min_score option to query

--- lib/searchkick/query.rb
+++ lib/searchkick/query.rb
@@ -47,6 +47,8 @@ module Searchkick
       padding = [options[:padding].to_i, 0].max
       offset = options[:offset] || (page - 1) * per_page + padding
 
+      min_score = options[:min_score] || 0
+
       # model and eagar loading
       load = options[:load].nil? ? true : options[:load]
 
@@ -222,7 +224,8 @@ module Searchkick
         payload = {
           query: payload,
           size: per_page,
-          from: offset
+          from: offset,
+          min_score: min_score
         }
         payload[:explain] = options[:explain] if options[:explain]
 
