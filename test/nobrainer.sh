dcbbe71e14c3861da45c5ac0039e364b8698772d
Andrew Kane <acekane1@gmail.com>
2014-11-18 21:56:25 -0800

    Updated RethinkDB install

--- test/nobrainer.sh
+++ test/nobrainer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ ! -z "$NOBRAINER" ]; then
+if [ -n "$NOBRAINER" ]; then
   source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
   wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
   sudo apt-get update -q
