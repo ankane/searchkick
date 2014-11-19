c8d1c40c38158107da4e138fb06640abf1f1679b
Andrew Kane <acekane1@gmail.com>
2014-11-18 21:52:11 -0800

    Only install RethinkDB when needed

---
+++ test/nobrainer.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+if [ ! -z "$NOBRAINER" ]; then
+  source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+  wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+  sudo apt-get update -q
+  sudo apt-get install rethinkdb
+  sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf
+  sudo service rethinkdb restart
+fi
