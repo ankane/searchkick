f48bb63bb0b57bd7ac66b39e7f1a3e007dd543ce
Jeremy Apthorp <nornagon@nornagon.net>
2015-01-21 09:28:20 -0800

    Allow '_all' to be used as a search field

--- lib/searchkick/query.rb
+++ lib/searchkick/query.rb
@@ -25,11 +25,15 @@ module Searchkick
             options[:fields].map{|f| "#{f}.autocomplete" }
           else
             options[:fields].map do |value|
-              k, v = value.is_a?(Hash) ? value.to_a.first : [value, :word]
-              k2, boost = k.to_s.split("^", 2)
-              field = "#{k2}.#{v == :word ? "analyzed" : v}"
-              boost_fields[field] = boost.to_i if boost
-              field
+              if value == '_all'
+                value
+              else
+                k, v = value.is_a?(Hash) ? value.to_a.first : [value, :word]
+                k2, boost = k.to_s.split("^", 2)
+                field = "#{k2}.#{v == :word ? "analyzed" : v}"
+                boost_fields[field] = boost.to_i if boost
+                field
+              end
             end
           end
         else
