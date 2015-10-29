581a26dce95966dfec3c0584009468a4a3ab8602
Andrew Kane <acekane1@gmail.com>
2015-10-28 23:54:05 -0700

    Run Elasticsearch in background

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -2,7 +2,7 @@
 
 sudo service elasticsearch stop
 curl -s https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.3.tar.gz | tar xz -C /tmp || true
-/tmp/elasticsearch-1.7.3/bin/elasticsearch
+/tmp/elasticsearch-1.7.3/bin/elasticsearch -d
 
 if [ -n "$NOBRAINER" ]; then
   source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
