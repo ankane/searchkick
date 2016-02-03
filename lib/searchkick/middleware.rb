b7f2e279fa4b2c5110e12360ea8c7eb6fef62b47
Aaron Craig <aaron@twenty20.com>
2016-02-02 22:09:07 -0800

    use hash interface

--- lib/searchkick/middleware.rb
+++ lib/searchkick/middleware.rb
@@ -3,21 +3,10 @@ require "faraday/middleware"
 module Searchkick
   class Middleware < Faraday::Middleware
     def call(env)
-      is_search = env_value(env, :url).path.to_s.end_with?("/_search")
-
-      if env_value(env, :method) == :get && is_search
-        r = env_value(env, :request)
-        if r.is_a?(Hash)
-          r[:timeout] = Searchkick.search_timeout
-        else
-          r.timeout = Searchkick.search_timeout
-        end
+      if env[:method] == :get && env[:url][:path].to_s.end_with?("/_search")
+        env[:request][:timeout] = Searchkick.search_timeout
       end
       @app.call(env)
-    end
-
-    def env_value(env, key)
-      env.is_a?(Hash) ? env[key] : env.send(key)
     end
   end
 end
