1d54d9b0a9d3e9299a5e02afcd1e9404aba2bd78
Aaron Craig <aaron@twenty20.com>
2016-02-02 22:21:09 -0800

    try treating :uri as a hash

--- lib/searchkick/middleware.rb
+++ lib/searchkick/middleware.rb
@@ -3,7 +3,7 @@ require "faraday/middleware"
 module Searchkick
   class Middleware < Faraday::Middleware
     def call(env)
-      if env[:method] == :get && env[:url][:path].to_s.end_with?("/_search")
+      if env[:method] == :get && env[:url].path.to_s.end_with?("/_search")
         env[:request][:timeout] = Searchkick.search_timeout
       end
       @app.call(env)
