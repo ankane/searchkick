b76037c60330ecf9e679fcb5df750a595e0b51c2
Jin Lee <jin.lee@bydeluxe.com>
2016-03-23 07:12:38 +0900

    mutex lock when 2 jobs want to index the same record

--- lib/searchkick/reindex_v2_job.rb
+++ lib/searchkick/reindex_v2_job.rb
@@ -2,7 +2,47 @@ module Searchkick
   class ReindexV2Job < ActiveJob::Base
     queue_as :searchkick
 
+    attr_accessor :key
+
+    def reindex_mutex
+      @@reindex_mutex ||= Mutex.new
+    end
+
+    def reindexes
+      @@reindexes ||= Hash.new
+    end
+
+    def reindex_add(klass, id)
+      reindex_mutex.synchronize do
+        if reindexes[key].nil?
+          reindexes[key] = { count: 1, mutex: Mutex.new }
+        else
+          reindexes[key][:count] += 1
+        end
+      end
+    end
+
+    def reindex_remove(klass, id)
+      reindex_mutex.synchronize do
+        reindexes[key][:count] -= 1
+        reindexes.delete key if reindexes[key][:count].zero?
+      end
+    end
+
+    def self.reindex_synchronize(klass, id)
+      reindexes[key][:mutex].synchronize do
+        yield
+      end
+    end
+
     def perform(klass, id)
+      self.key = "#{klass}::#{id}"
+      reindex_add klass, id
+      reindex_synchronize { reindex_model klass, id }
+      reindex_remove klass, id
+    end
+
+    def reindex_model(klass, id)
       model = klass.constantize
       record = model.find(id) rescue nil # TODO fix lazy coding
       index = model.searchkick_index
