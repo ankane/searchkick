1f265debb71102330237cf29ac21b95235b0e2ce
Andrew Kane <acekane1@gmail.com>
2016-07-24 15:29:29 -0700

    Better fix for failing test

--- test/errors_test.rb
+++ test/errors_test.rb
@@ -2,8 +2,8 @@ require_relative "test_helper"
 
 class ErrorsTest < Minitest::Test
   def test_bulk_import_raises_with_full_message
-    valid_dog = Dog.new(name: "2016-01-02")
-    invalid_dog = Dog.new(name: "Ol' One-Leg")
+    valid_dog = Dog.new(id: 1, name: "2016-01-02")
+    invalid_dog = Dog.new(id: 2, name: "Ol' One-Leg")
     index = Searchkick::Index.new "dogs", mappings: {
       dog: {
         properties: {
@@ -15,5 +15,6 @@ class ErrorsTest < Minitest::Test
     error = assert_raises(Searchkick::ImportError) do
       index.bulk_index [valid_dog, invalid_dog]
     end
+    assert_match /on item with id '2'/, error.message
   end
 end
