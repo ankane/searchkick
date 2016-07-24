070621abb2e8e6463b3c97302f0aa5e9d0c1d5c8
Andrew Kane <acekane1@gmail.com>
2016-07-24 15:46:13 -0700

    Fixed tests

--- test/errors_test.rb
+++ test/errors_test.rb
@@ -1,9 +1,9 @@
 require_relative "test_helper"
 
 class ErrorsTest < Minitest::Test
-  def test_bulk_import_raises_with_full_message
-    valid_dog = Dog.new(id: 1, name: "2016-01-02")
-    invalid_dog = Dog.new(id: 2, name: "Ol' One-Leg")
+  def test_bulk_import_raises_error
+    valid_dog = Dog.new(name: "2016-01-02")
+    invalid_dog = Dog.new(name: "Ol' One-Leg")
     index = Searchkick::Index.new "dogs", mappings: {
       dog: {
         properties: {
@@ -15,6 +15,5 @@ class ErrorsTest < Minitest::Test
     error = assert_raises(Searchkick::ImportError) do
       index.bulk_index [valid_dog, invalid_dog]
     end
-    assert_match /on item with id '2'/, error.message
   end
 end
