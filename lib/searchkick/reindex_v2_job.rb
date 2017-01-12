8985e2002efb38e233fd6faea1c4750aa5536cd8
Andrew Kane <acekane1@gmail.com>
2017-01-12 00:41:25 -0800

    DRY background job

--- lib/searchkick/reindex_v2_job.rb
+++ lib/searchkick/reindex_v2_job.rb
@@ -20,19 +20,12 @@ module Searchkick
           nil
         end
 
-      index = model.searchkick_index
-      if !record || !record.should_index?
-        # hacky
+      unless record
         record ||= model.new
         record.id = id
-        begin
-          index.remove record
-        rescue Elasticsearch::Transport::Transport::Errors::NotFound
-          # do nothing
-        end
-      else
-        index.store record
       end
+
+      model.searchkick_index.reindex_record(record)
     end
   end
 end
