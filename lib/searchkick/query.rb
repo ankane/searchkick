fb9d24ae1734a9779bdc4f4049f76d7b18a9a8d9
Andrew <acekane1@gmail.com>
2018-04-03 20:17:39 -0700

    Fixed debug for Mongoid

--- lib/searchkick/query.rb
+++ lib/searchkick/query.rb
@@ -135,7 +135,7 @@ module Searchkick
         if searchkick_index
           puts "Model Search Data"
           begin
-            pp klass.first(3).map { |r| RecordData.new(searchkick_index, r).index_data }
+            pp klass.limit(3).map { |r| RecordData.new(searchkick_index, r).index_data }
           rescue => e
             puts "#{e.class.name}: #{e.message}"
           end
