28f1fb2da2fdc811351e80916e5b71f799cd21f6
David Sweetman <david@davidsweetman.com>
2018-05-14 21:38:00 -0400

    Add an assert_misspellings test helper

--- test/per_field_misspellings_test.rb
+++ test/per_field_misspellings_test.rb
@@ -1,147 +1,99 @@
 require_relative "test_helper"
 
 class FieldMisspellingTest < Minitest::Test
-  def test_correct_spelling_still_works
+  def test_misspellings_field_correct_spelling_still_works
     store [{name: "Sriracha", color: "blue"}]
-    params = {
-      fields: [:name, :color],
-      misspellings: { fields: { name: false, color: false } }
-    }
-    assert_search "Sriracha", ["Sriracha"], params
-    assert_search "blue", ["Sriracha"], params
+    assert_misspellings "Sriracha", ["Sriracha"], {fields: {name: false, color: false}}
+    assert_misspellings "blue", ["Sriracha"], {fields: {name: false, color: false }}
   end
 
-  def test_misspelling_enabled
+  def test_misspellings_field_enabled
     store [{name: "Sriracha", color: "blue"}]
-    assert_search "siracha", ["Sriracha"],
-      fields: [:name, :color],
-      misspellings: { fields: { name: true } }
-    assert_search "clue", ["Sriracha"],
-      fields: [:name, :color],
-      misspellings: { fields: { color: true } }
+    assert_misspellings "siracha", ["Sriracha"], {fields: {name: true}}
+    assert_misspellings "clue", ["Sriracha"], {fields: {color: true}}
   end
 
-  def test_misspelling_disabled
+  def test_misspellings_field_disabled
     store [{name: "Sriracha", color: "blue"}]
-    assert_search "siracha", [],
-      fields: [:name, :color],
-      misspellings: { fields: { name: false } }
-    assert_search "clue", [],
-      fields: [:name, :color],
-      misspellings: { fields: { color: false } }
+    assert_misspellings "siracha", [], {fields: {name: false}}
+    assert_misspellings "clue", [], {fields: {color: false}}
   end
 
-  def test_misspelling_color
+  def test_misspellings_field__color
     store [{name: "Sriracha", color: "blue"}]
-    assert_search "bluu", ["Sriracha"],
-      fields: [:name, :color],
-      misspellings: { fields: { name: false, color: true } }
+    assert_misspellings "bluu", ["Sriracha"], {fields: {name: false, color: true}}
   end
 
-  def test_misspelling_multiple
+  def test_misspellings_field_multiple
     store [
       {name: "Greek Yogurt", color: "white"},
       {name: "Green Onions", color: "yellow"}
     ]
-    assert_search "greed", ["Greek Yogurt", "Green Onions"],
-      fields: [:name, :color],
-      misspellings: { fields: { name: true, color: false } }
-    assert_search "greed", [],
-      fields: [:name, :color],
-      misspellings: { fields: { name: false, color: true } }
+    assert_misspellings "greed", ["Greek Yogurt", "Green Onions"], {fields: {name: true, color: false}}
+    assert_misspellings "greed", [], {fields: {name: false, color: true}}
   end
 
-  def test_unspecified_field_uses_edit_distance_one
+  def test_misspellings_field_unspecified_uses_edit_distance_one
     store [{name: "Bingo", color: "blue"}]
-    assert_search "bin", [],
-      fields: [:name, :color],
-      misspellings: { fields: { color: { edit_distance: 2 } } }
-    assert_search "bingooo", [],
-      fields: [:name, :color],
-      misspellings: { fields: { color: { edit_distance: 2 } } }
-    assert_search "mango", [],
-      fields: [:name, :color],
-      misspellings: { fields: { color: { edit_distance: 2 } } }
-    assert_search "bing", ["Bingo"],
-      fields: [:name, :color],
-      misspellings: { fields: { color: { edit_distance: 2 } } }
+    assert_misspellings "bin", [], {fields: {color: {edit_distance: 2}}}
+    assert_misspellings "bingooo", [], {fields: {color: {edit_distance: 2}}}
+    assert_misspellings "mango", [], {fields: {color: {edit_distance: 2}}}
+    assert_misspellings "bing", ["Bingo"], {fields: {color: {edit_distance: 2}}}
   end
 
-  def test_field_uses_specified_edit_distance
+  def test_misspellings_field_uses_specified_edit_distance
     store [{name: "Bingo", color: "yellow"}]
-    assert_search "yell", ["Bingo"],
-      fields: [:name, :color],
-      misspellings: { fields: { color: { edit_distance: 2 } } }
-    assert_search "yellowww", ["Bingo"],
-      fields: [:name, :color],
-      misspellings: { fields: { color: { edit_distance: 2 } } }
-    assert_search "yilliw", ["Bingo"],
-      fields: [:name, :color],
-      misspellings: { fields: { color: { edit_distance: 2 } } }
+    assert_misspellings "yell", ["Bingo"], {fields: {color: {edit_distance: 2}}}
+    assert_misspellings "yellowww", ["Bingo"], {fields: {color: {edit_distance: 2}}}
+    assert_misspellings "yilliw", ["Bingo"], {fields: {color: {edit_distance: 2}}}
   end
 
-  def test_misspelling_zucchini_transposition
+  def test_misspellings_field_zucchini_transposition
     store [{name: "zucchini", color: "green"}]
-    assert_search "zuccihni", [],
-      fields: [:name, :color],
-      misspellings: { fields: { name: {transpositions: false} } }
-    assert_search "grene", ["zucchini"],
-      fields: [:name, :color],
-      misspellings: { fields: { name: {transpositions: false} } }
+    assert_misspellings "zuccihni", [], {fields: {name: {transpositions: false}}}
+    assert_misspellings "grene", ["zucchini"], {fields: {name: {transpositions: false}}}
   end
 
-  def test_misspellings_transposition_combination
+  def test_misspellings_field_transposition_combination
     store [{name: "zucchini", color: "green"}]
-    options = {
-      fields: [:name, :color],
-      misspellings: {
+    misspellings = {
         transpositions: false,
         fields: {color: {transpositions: true}}
-      }
     }
-    assert_search "zuccihni", [], options
-    assert_search "greene", ["zucchini"], options
+    assert_misspellings "zuccihni", [], misspellings
+    assert_misspellings "greene", ["zucchini"], misspellings
   end
 
-  def test_misspellings_word_start
+  def test_misspellings_field_word_start
     store_names ["Sriracha"]
-    assert_search "siracha", ["Sriracha"],
-      fields: [{name: :word_start}],
-      misspellings: { fields: { name: true } }
+    assert_misspellings "siracha", ["Sriracha"], {fields: {name: true}}
   end
 
-  def test_misspellings_fields_and_transpositions
+  def test_misspellings_field_and_transpositions
     store [{name: "Sriracha", color: "green"}]
     options = {
       fields: [{name: :word_start}],
       misspellings: {
         transpositions: false,
-        fields: { name: true }
+        fields: {name: true}
       }
     }
     assert_search "grene", [], options
     assert_search "srircaha", ["Sriracha"], options
   end
 
-  def test_misspellings_fields_and_edit_distance
+  def test_misspellings_field_and_edit_distance
     store [{name: "Sriracha", color: "green"}]
-    options = {
-      fields: [:name, :color],
-      misspellings: {
-        edit_distance: 2,
-        fields: { name: true }
-      }
-    }
-    assert_search "greennn", ["Sriracha"], options
-    assert_search "srirachaaa", [], options
-    assert_search "siracha", ["Sriracha"], options
+    options = {edit_distance: 2, fields: {name: true}}
+    assert_misspellings "greennn", ["Sriracha"], options
+    assert_misspellings "srirachaaa", [], options
+    assert_misspellings "siracha", ["Sriracha"], options
   end
 
-  def text_misspellings_fields_requires_explicit_search_fields
+  def test_misspellings_field_requires_explicit_search_fields
     store_names ["Sriracha"]
     assert_raises(ArgumentError) do
-      assert_search "siracha", ["Sriracha"],
-        misspellings: { fields: { name: false } }
+      assert_search "siracha", ["Sriracha"], {misspellings: {fields: {name: false}}}
     end
   end
 end
