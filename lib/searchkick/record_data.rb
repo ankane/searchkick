532ab59e4528c66f6ef7f9285c50c98a4c898edb
吉翔 <jx4554@gmail.com>
2018-06-07 23:23:01 +0800

    fix: prevent generate duplicate field (string/symbol)

--- lib/searchkick/record_data.rb
+++ lib/searchkick/record_data.rb
@@ -61,8 +61,8 @@ module Searchkick
       # hack to prevent generator field doesn't exist error
       if !partial_reindex
         index.suggest_fields.each do |field|
-          if !source[field] && !source[field.to_sym]
-            source[field] = nil
+          if (source.keys & [field, field.to_sym]).empty?
+	          source[field] = nil
           end
         end
       end
