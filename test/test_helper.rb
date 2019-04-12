bab97d5a9595ee38d4dda83318edb04c3f98b970
Andrew Kane <andrew@chartkick.com>
2019-04-11 21:34:29 -0700

    Wordnet test [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,9 @@ Searchkick.client.transport.logger = $logger
 Searchkick.search_timeout = 5
 Searchkick.index_suffix = ENV["TEST_ENV_NUMBER"] # for parallel tests
 
+# add to elasticsearch-7.0.0/config/
+Searchkick.wordnet_path = "wn_s.pl" if ENV["WORDNET"]
+
 puts "Running against Elasticsearch #{Searchkick.server_version}"
 
 if defined?(Redis)
