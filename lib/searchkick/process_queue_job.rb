d56fa7ab56a55f3c69255e96cb556a8b9624dbb1
Andrew Kane <andrew@chartkick.com>
2019-07-04 05:35:47 -0400

    Cleaner queue code [skip ci]

--- lib/searchkick/process_queue_job.rb
+++ lib/searchkick/process_queue_job.rb
@@ -4,19 +4,18 @@ module Searchkick
 
     def perform(class_name:)
       model = class_name.constantize
-
       limit = model.searchkick_index.options[:batch_size] || 1000
-      record_ids = model.searchkick_index.reindex_queue.reserve(limit: limit)
-      if record_ids.any?
-        Searchkick::ProcessBatchJob.perform_later(
-          class_name: model.name,
-          record_ids: record_ids
-        )
-        # TODO when moving to reliable queuing, mark as complete
 
-        if record_ids.size == limit
-          Searchkick::ProcessQueueJob.perform_later(class_name: class_name)
+      loop do
+        record_ids = model.searchkick_index.reindex_queue.reserve(limit: limit)
+        if record_ids.any?
+          Searchkick::ProcessBatchJob.perform_later(
+            class_name: model.name,
+            record_ids: record_ids
+          )
+          # TODO when moving to reliable queuing, mark as complete
         end
+        break unless record_ids.size == limit
       end
     end
   end
