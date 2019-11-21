d9cd91e6a3c70d3e361626103b469a9bcb0566eb
Steve Hodges <shodges317@gmail.com>
2019-11-21 16:21:37 -0500

    ECA-5119 indexed_at within search data
    Fix version number before release

--- lib/searchkick/record_data.rb
+++ lib/searchkick/record_data.rb
@@ -11,13 +11,13 @@ module Searchkick
 
     def index_data
       data = record_data
-      data[:data] = search_data
+      data[:data] = timestamped_search_data
       {index: data}
     end
 
     def update_data(method_name)
       data = record_data
-      data[:data] = {doc: search_data(method_name)}
+      data[:data] = {doc: timestamped_search_data(method_name)}
       {update: data}
     end
 
@@ -45,6 +45,12 @@ module Searchkick
     end
 
     private
+
+    def timestamped_search_data(method_name = nil)
+      search_data(method_name).merge(
+        indexed_at: Time.now.utc
+      )
+    end
 
     def search_data(method_name = nil)
       partial_reindex = !method_name.nil?
