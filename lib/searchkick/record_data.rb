80f73a6ab7df43e2e250cd422fd7c0d4ac91e76e
Steve Hodges <shodges317@gmail.com>
2019-11-21 16:21:37 -0500

    ECA-5119: Add indexed_at to search data so we can see when an record was last indexed

--- lib/searchkick/record_data.rb
+++ lib/searchkick/record_data.rb
@@ -11,13 +11,13 @@ module Searchkick
 
     def index_data
       data = record_data
-      data[:data] = search_data
+      data[:data] = timestamped_search_data
       {index: data}
     end
 
     def update_data(method_name)
       data = record_data
-      data[:data] = {doc: search_data(method_name)}
+      data[:data] = {doc: timestamped_search_data(method_name)}
       {update: data}
     end
 
@@ -44,6 +44,12 @@ module Searchkick
       }
       data[:_routing] = record.search_routing if record.respond_to?(:search_routing)
       data
+    end
+
+    def timestamped_search_data(method_name = nil)
+      search_data(method_name).merge(
+        indexed_at: Time.now.utc
+      )
     end
 
     def search_data(method_name = nil)
--- lib/searchkick/version.rb
+++ lib/searchkick/version.rb
@@ -1,3 +1,3 @@
 module Searchkick
-  VERSION = "3.2.0-everfi.1"
+  VERSION = "3.2.0-everfi.2"
 end
