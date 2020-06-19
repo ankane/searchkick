a07a87ca8ac2ed7c8f7e44f23635f4b592793b5d
Andrew Kane <andrew@chartkick.com>
2020-06-18 19:44:31 -0700

    DRY test [skip ci]

--- test/index_options_test.rb
+++ test/index_options_test.rb
@@ -7,22 +7,26 @@ class IndexOptionsTest < Minitest::Test
 
   def test_case_sensitive
     with_options(Song, case_sensitive: true) do
-      store_names ["Test", "test"], Song
-      assert_search "test", ["test"], {misspellings: false}, Song
+      store_names ["Test", "test"]
+      assert_search "test", ["test"], {misspellings: false}
     end
   end
 
   def test_no_stemming
     with_options(Song, stem: false) do
-      store_names ["milk", "milks"], Song
-      assert_search "milks", ["milks"], {misspellings: false}, Song
+      store_names ["milk", "milks"]
+      assert_search "milks", ["milks"], {misspellings: false}
     end
   end
 
   def test_special_characters
     with_options(Song, special_characters: false) do
-      store_names ["jalapeño"], Song
-      assert_search "jalapeno", [], {misspellings: false}, Song
+      store_names ["jalapeño"]
+      assert_search "jalapeno", [], {misspellings: false}
     end
+  end
+
+  def default_model
+    Song
   end
 end
