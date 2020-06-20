83fe00bcf7688cf9ed8a6a96a2fc18e4e6835704
Andrew Kane <andrew@chartkick.com>
2020-06-20 06:58:10 -0700

    Use default model [skip ci]

--- test/multi_tenancy_test.rb
+++ test/multi_tenancy_test.rb
@@ -7,16 +7,20 @@ class MultiTenancyTest < Minitest::Test
 
   def test_basic
     Apartment::Tenant.switch!("tenant1")
-    store_names ["Product A"], Tenant
+    store_names ["Product A"]
     Apartment::Tenant.switch!("tenant2")
-    store_names ["Product B"], Tenant
+    store_names ["Product B"]
     Apartment::Tenant.switch!("tenant1")
-    assert_search "product", ["Product A"], {load: false}, Tenant
+    assert_search "product", ["Product A"], {load: false}
     Apartment::Tenant.switch!("tenant2")
-    assert_search "product", ["Product B"], {load: false}, Tenant
+    assert_search "product", ["Product B"], {load: false}
   end
 
   def teardown
     Apartment::Tenant.reset if defined?(Apartment)
+  end
+
+  def default_model
+    Tenant
   end
 end
