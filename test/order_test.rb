96c70fefec6d996dcfbaa5406fbbd8862de901a0
Andrew Kane <andrew@chartkick.com>
2020-06-20 05:19:59 -0700

    Updated test names [skip ci]

--- test/order_test.rb
+++ test/order_test.rb
@@ -1,17 +1,17 @@
 require_relative "test_helper"
 
 class OrderTest < Minitest::Test
-  def test_order_hash
+  def test_hash
     store_names ["Product A", "Product B", "Product C", "Product D"]
     assert_order "product", ["Product D", "Product C", "Product B", "Product A"], order: {name: :desc}
   end
 
-  def test_order_string
+  def test_string
     store_names ["Product A", "Product B", "Product C", "Product D"]
     assert_order "product", ["Product A", "Product B", "Product C", "Product D"], order: "name"
   end
 
-  def test_order_id
+  def test_id
     skip if cequel?
 
     store_names ["Product A", "Product B"]
@@ -20,7 +20,7 @@ class OrderTest < Minitest::Test
     assert_order "product", [product_a, product_b].sort_by { |r| r.id.to_s }.map(&:name), order: {id: :asc}
   end
 
-  def test_order_multiple
+  def test_multiple
     store [
       {name: "Product A", color: "blue", store_id: 1},
       {name: "Product B", color: "red", store_id: 3},
@@ -29,11 +29,11 @@ class OrderTest < Minitest::Test
     assert_order "product", ["Product A", "Product B", "Product C"], order: {color: :asc, store_id: :desc}
   end
 
-  def test_order_unmapped_type
+  def test_unmapped_type
     assert_order "product", [], order: {not_mapped: {unmapped_type: "long"}}
   end
 
-  def test_order_array
+  def test_array
     store [{name: "San Francisco", latitude: 37.7833, longitude: -122.4167}]
     assert_order "francisco", ["San Francisco"], order: [{_geo_distance: {location: "0,0"}}]
   end
