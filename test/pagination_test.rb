86528307359692b8e983f2c682fb69706650cf24
Andrew Kane <andrew@chartkick.com>
2020-11-02 19:17:50 -0800

    Test with Rails 6.1 [skip ci]

--- test/pagination_test.rb
+++ test/pagination_test.rb
@@ -88,7 +88,8 @@ class PaginationTest < Minitest::Test
     I18n.load_path = Dir["test/support/kaminari.yml"]
     I18n.backend.load_translations
 
-    view = ActionView::Base.new(ActionView::LookupContext.new([]))
+    view_args = [[], nil] if ActionView::VERSION::STRING.to_f >= 6.1
+    view = ActionView::Base.new(ActionView::LookupContext.new([]), *view_args)
 
     store_names ["Product A"]
     assert_equal "Displaying <b>1</b> product", view.page_entries_info(Product.search("product"))
