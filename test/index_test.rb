dd85a230037105560804dedb6089e47833f09d95
Andrew Kane <andrew@ankane.org>
2021-08-10 17:28:03 -0700

    Moved test [skip ci]

--- test/index_test.rb
+++ test/index_test.rb
@@ -105,4 +105,21 @@ class IndexTest < Minitest::Test
     # values that exceed ignore_above are not included in _all field :(
     # assert_search "hello", ["Product A"], {}, Region
   end
+
+  def test_bulk_import_raises_error
+    valid_dog = Product.create(name: "2016-01-02")
+    invalid_dog = Product.create(name: "Ol' One-Leg")
+    mapping = {
+      properties: {
+        name: {type: "date"}
+      }
+    }
+    index = Searchkick::Index.new "dogs", mappings: mapping, _type: "dog"
+    index.delete if index.exists?
+    index.create_index
+    index.store valid_dog
+    assert_raises(Searchkick::ImportError) do
+      index.bulk_index [valid_dog, invalid_dog]
+    end
+  end
 end
