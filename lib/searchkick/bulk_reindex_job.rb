a600122c3fa15bd2c51a9132426bdaf3f111e55d
Yasoob Ahmad <110978512+Yasoob01@users.noreply.github.com>
2025-03-04 01:11:35 +0500

    Bulk Reindexing Optimization with custom_import_scope for Faster Partial Indexing

--- lib/searchkick/bulk_reindex_job.rb
+++ lib/searchkick/bulk_reindex_job.rb
@@ -3,7 +3,7 @@ module Searchkick
     queue_as { Searchkick.queue_name }
 
     # TODO remove min_id and max_id in Searchkick 6
-    def perform(class_name:, record_ids: nil, index_name: nil, method_name: nil, batch_id: nil, min_id: nil, max_id: nil)
+    def perform(class_name:, record_ids: nil, index_name: nil, method_name: nil, batch_id: nil, min_id: nil, max_id: nil, custom_import_scope: nil)
       model = Searchkick.load_model(class_name)
       index = model.searchkick_index(name: index_name)
 
@@ -12,7 +12,15 @@ module Searchkick
 
       relation = Searchkick.scope(model)
       relation = Searchkick.load_records(relation, record_ids)
-      relation = relation.search_import if relation.respond_to?(:search_import)
+
+      relation = if custom_import_scope.present?
+                   relation.includes(custom_import_scope)
+                  elsif relation.respond_to?(:search_import)
+                    relation.search_import 
+                  else
+                    relation
+                  end
+
 
       RecordIndexer.new(index).reindex(relation, mode: :inline, method_name: method_name, full: false)
       RelationIndexer.new(index).batch_completed(batch_id) if batch_id
