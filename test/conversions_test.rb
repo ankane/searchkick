e2eb7befa5f08bb52edad6db975ea93fe93d43e5
Andrew Kane <andrew@ankane.org>
2025-10-05 18:27:52 -0700

    Improved tests for conversions_v2 [skip ci]

--- test/conversions_test.rb
+++ test/conversions_test.rb
@@ -6,7 +6,7 @@ class ConversionsTest < Minitest::Test
     setup_speaker
   end
 
-  def test_conversions
+  def test_v1
     store [
       {name: "Tomato A", conversions: {"tomato" => 1}},
       {name: "Tomato B", conversions: {"tomato" => 2}},
@@ -16,52 +16,7 @@ class ConversionsTest < Minitest::Test
     assert_equal_scores "tomato", conversions: false
   end
 
-  def test_conversions_v2
-    store [
-      {name: "Tomato A", conversions_v2: {"tomato" => 1}},
-      {name: "Tomato B", conversions_v2: {"tomato" => 2}},
-      {name: "Tomato C", conversions_v2: {"tomato" => 3}}
-    ]
-    assert_order "tomato", ["Tomato C", "Tomato B", "Tomato A"], conversions_v2: [:conversions_v2]
-  end
-
-  def test_conversions_v2_space
-    store [
-      {name: "Tomato A", conversions_v2: {"tomato juice" => 1}},
-      {name: "Tomato B", conversions_v2: {"tomato juice" => 2}},
-      {name: "Tomato C", conversions_v2: {"tomato juice" => 3}}
-    ]
-    assert_order "tomato", ["Tomato C", "Tomato B", "Tomato A"], conversions_v2: [:conversions_v2], conversions_term: "tomato juice"
-  end
-
-  def test_conversions_v2_dot
-    store [
-      {name: "Tomato A", conversions_v2: {"tomato.juice" => 1}},
-      {name: "Tomato B", conversions_v2: {"tomato.juice" => 2}},
-      {name: "Tomato C", conversions_v2: {"tomato.juice" => 3}}
-    ]
-    assert_order "tomato", ["Tomato C", "Tomato B", "Tomato A"], conversions_v2: [:conversions_v2], conversions_term: "tomato.juice"
-  end
-
-  def test_conversions_v2_zero
-    error = assert_raises(Searchkick::ImportError) do
-      store [{name: "Tomato A", conversions_v2: {"tomato" => 0}}]
-    end
-    assert_match "must be a positive normal float", error.message
-  end
-
-  def test_conversions_v2_score
-    store [
-      {name: "Tomato A", conversions: {"tomato" => 1}, conversions_v2: {"tomato" => 1}},
-      {name: "Tomato B", conversions: {"tomato" => 2}, conversions_v2: {"tomato" => 2}},
-      {name: "Tomato C", conversions: {"tomato" => 3}, conversions_v2: {"tomato" => 3}}
-    ]
-    scores = Product.search("tomato", conversions_v2: false, load: false).map(&:_score)
-    scores_v2 = Product.search("tomato", conversions: false, conversions_v2: "conversions_v2", load: false).map(&:_score)
-    assert_equal scores, scores_v2
-  end
-
-  def test_multiple_conversions
+  def test_v1_multiple_conversions
     store [
       {name: "Speaker A", conversions_a: {"speaker" => 1}, conversions_b: {"speaker" => 6}},
       {name: "Speaker B", conversions_a: {"speaker" => 2}, conversions_b: {"speaker" => 5}},
@@ -76,7 +31,7 @@ class ConversionsTest < Minitest::Test
     assert_order "speaker", ["Speaker A", "Speaker B", "Speaker C"], {conversions: "conversions_b"}, Speaker
   end
 
-  def test_multiple_conversions_with_boost_term
+  def test_v1_multiple_conversions_with_boost_term
     store [
       {name: "Speaker A", conversions_a: {"speaker" => 4, "speaker_1" => 1}},
       {name: "Speaker B", conversions_a: {"speaker" => 3, "speaker_1" => 2}},
@@ -88,7 +43,7 @@ class ConversionsTest < Minitest::Test
     assert_order "speaker", ["Speaker D", "Speaker C", "Speaker B", "Speaker A"], {conversions: "conversions_a", conversions_term: "speaker_1"}, Speaker
   end
 
-  def test_conversions_case
+  def test_v1_case
     store [
       {name: "Tomato A", conversions: {"tomato" => 1, "TOMATO" => 1, "tOmAtO" => 1}},
       {name: "Tomato B", conversions: {"tomato" => 2}}
@@ -96,12 +51,93 @@ class ConversionsTest < Minitest::Test
     assert_order "tomato", ["Tomato A", "Tomato B"]
   end
 
-  def test_conversions_weight
+  def test_v1_weight
     Product.reindex
     store [
       {name: "Product Boost", orders_count: 20},
       {name: "Product Conversions", conversions: {"product" => 10}}
     ]
     assert_order "product", ["Product Conversions", "Product Boost"], boost: "orders_count"
+  end
+
+  def test_v2
+    store [
+      {name: "Tomato A", conversions_v2: {"tomato" => 1}},
+      {name: "Tomato B", conversions_v2: {"tomato" => 2}},
+      {name: "Tomato C", conversions_v2: {"tomato" => 3}}
+    ]
+    assert_order "tomato", ["Tomato C", "Tomato B", "Tomato A"], conversions_v2: [:conversions_v2]
+  end
+
+  def test_v2_space
+    store [
+      {name: "Tomato A", conversions_v2: {"tomato juice" => 1}},
+      {name: "Tomato B", conversions_v2: {"tomato juice" => 2}},
+      {name: "Tomato C", conversions_v2: {"tomato juice" => 3}}
+    ]
+    assert_order "tomato", ["Tomato C", "Tomato B", "Tomato A"], conversions_v2: [:conversions_v2], conversions_term: "tomato juice"
+  end
+
+  def test_v2_dot
+    store [
+      {name: "Tomato A", conversions_v2: {"tomato.juice" => 1}},
+      {name: "Tomato B", conversions_v2: {"tomato.juice" => 2}},
+      {name: "Tomato C", conversions_v2: {"tomato.juice" => 3}}
+    ]
+    assert_order "tomato", ["Tomato C", "Tomato B", "Tomato A"], conversions_v2: [:conversions_v2], conversions_term: "tomato.juice"
+  end
+
+  def test_v2_unicode
+    store [
+      {name: "Tomato A", conversions_v2: {"喰らう" => 1}},
+      {name: "Tomato B", conversions_v2: {"喰らう" => 2}},
+      {name: "Tomato C", conversions_v2: {"喰らう" => 3}}
+    ]
+    assert_order "tomato", ["Tomato C", "Tomato B", "Tomato A"], conversions_v2: [:conversions_v2], conversions_term: "喰らう"
+  end
+
+  def test_v2_score
+    store [
+      {name: "Tomato A", conversions: {"tomato" => 1}, conversions_v2: {"tomato" => 1}},
+      {name: "Tomato B", conversions: {"tomato" => 2}, conversions_v2: {"tomato" => 2}},
+      {name: "Tomato C", conversions: {"tomato" => 3}, conversions_v2: {"tomato" => 3}}
+    ]
+    scores = Product.search("tomato", conversions_v2: false, load: false).map(&:_score)
+    scores_v2 = Product.search("tomato", conversions: false, conversions_v2: "conversions_v2", load: false).map(&:_score)
+    assert_equal scores, scores_v2
+  end
+
+  def test_v2_no_tokenization
+    store [
+      {name: "Tomato A"},
+      {name: "Tomato B", conversions_v2: {"tomato juice" => 2}},
+      {name: "Tomato C", conversions_v2: {"tomato vine" => 3}}
+    ]
+    assert_equal_scores "tomato", conversions_v2: [:conversions_v2]
+  end
+
+  def test_v2_max_conversions
+    conversions = 66000.times.to_h { |i| ["term#{i}", 1] }
+    store [{name: "Tomato A", conversions_v2: conversions}]
+
+    conversions.merge!(1000.times.to_h { |i| ["term#{conversions.size + i}", 1] })
+    assert_raises(Searchkick::ImportError) do
+      store [{name: "Tomato B", conversions_v2: conversions}]
+    end
+  end
+
+  def test_v2_max_length
+    store [{name: "Tomato A", conversions_v2: {"a"*32766 => 1}}]
+
+    assert_raises(Searchkick::ImportError) do
+      store [{name: "Tomato B", conversions_v2: {"a"*32767 => 1}}]
+    end
+  end
+
+  def test_v2_zero
+    error = assert_raises(Searchkick::ImportError) do
+      store [{name: "Tomato A", conversions_v2: {"tomato" => 0}}]
+    end
+    assert_match "must be a positive normal float", error.message
   end
 end
