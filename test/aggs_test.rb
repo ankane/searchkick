9d7c0a41d83ba084abe34f3b29be0da042916b05
Andrew Kane <andrew@ankane.org>
2026-02-12 16:13:17 -0800

    Updated style [skip ci]

--- test/aggs_test.rb
+++ test/aggs_test.rb
@@ -115,23 +115,21 @@ class AggsTest < Minitest::Test
 
   def test_aggs_with_time_zone
     start_time = Time.at(1529366400)
-
     store [
       {name: "Opera House Pass", created_at: start_time},
       {name: "London Eye Pass", created_at: start_time + 16.hours},
       {name: "London Tube Pass", created_at: start_time + 16.hours}
     ]
 
-    sydney_search = search_aggregate_by_day_with_time_zone('Pass', '+10:00') # Sydney
-    london_search = search_aggregate_by_day_with_time_zone('Pass', '+01:00') # London
-
     # London search will return all 3 in one bucket because of time zone offset
+    london_search = search_aggregate_by_day_with_time_zone("+01:00")
     expected_london_buckets = [
       {"key_as_string" => "2018-06-19T00:00:00.000+01:00", "key" => 1529362800000, "doc_count" => 3}
     ]
     assert_equal expected_london_buckets, london_search.aggs["products_per_day"]["buckets"]
 
     # Sydney search will return them in separate buckets due to time zone offset
+    sydney_search = search_aggregate_by_day_with_time_zone("+10:00")
     expected_sydney_buckets = [
       {"key_as_string" => "2018-06-19T00:00:00.000+10:00", "key" => 1529330400000, "doc_count" => 1},
       {"key_as_string" => "2018-06-20T00:00:00.000+10:00", "key" => 1529416800000, "doc_count" => 2}
@@ -180,15 +178,15 @@ class AggsTest < Minitest::Test
     assert_equal expected, relation.aggs.to_h { |field, agg| [field, buckets_as_hash(agg)] }
   end
 
-  def search_aggregate_by_day_with_time_zone(query, time_zone = '-8:00')
+  def buckets_as_hash(agg)
+    agg["buckets"].to_h { |v| [v["key"], v["doc_count"]] }
+  end
+
+  def search_aggregate_by_day_with_time_zone(time_zone)
     Product.search(
-      query,
+      "Pass",
       where: {created_at: {lt: Time.now}},
       aggs: {products_per_day: {date_histogram: {field: :created_at, calendar_interval: :day, time_zone: time_zone}}}
     )
-  end
-
-  def buckets_as_hash(agg)
-    agg["buckets"].to_h { |v| [v["key"], v["doc_count"]] }
   end
 end
