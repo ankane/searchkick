eb06813c8c492f65e6f783969e2ff4e6473fd64d
Andrew Kane <andrew@ankane.org>
2026-02-17 00:30:01 -0800

    Added more tests for smart_aggs [skip ci]

--- test/aggs_test.rb
+++ test/aggs_test.rb
@@ -155,13 +155,25 @@ class AggsTest < Minitest::Test
 
   def test_smart_aggs
     assert_aggs ({"store_id" => {1 => 1, 2 => 2}}), where: {store_id: 2}, aggs: [:store_id]
+    assert_aggs ({"store_id" => {1 => 1, 2 => 2}}), where: {store_id: {not: 2}}, aggs: [:store_id]
+    # TODO fix
+    assert_aggs ({"store_id" => {1 => 1}}), where: {_not: {store_id: 2}}, aggs: [:store_id]
+    # TODO fix
+    assert_aggs ({"store_id" => {2 => 2}}), where: {_and: [{store_id: 2}]}, aggs: [:store_id]
+
     assert_aggs ({"store_id" => {1 => 1, 2 => 1}}), where: {store_id: 2, price: {gt: 5}}, aggs: [:store_id]
   end
 
   def test_smart_aggs_relation
     assert_aggs ({"store_id" => {1 => 1, 2 => 2}}), Product.search("Product").where(store_id: 2).aggs(:store_id)
-    assert_aggs ({"store_id" => {1 => 1, 2 => 1}}), Product.search("Product").where(store_id: 2, price: {gt: 5}).aggs(:store_id)
+    assert_aggs ({"store_id" => {1 => 1, 2 => 2}}), Product.search("Product").where(store_id: {not: 2}).aggs(:store_id)
+    # TODO fix
+    assert_aggs ({"store_id" => {1 => 1}}), Product.search("Product").where.not(store_id: 2).aggs(:store_id)
+    # TODO fix
+    assert_aggs ({"store_id" => {2 => 2}}), Product.search("Product").where(_and: [{store_id: 2}]).aggs(:store_id)
+
     assert_aggs ({"store_id" => {1 => 1, 2 => 1}}), Product.search("Product").where(store_id: 2).where(price: {gt: 5}).aggs(:store_id)
+    assert_aggs ({"store_id" => {1 => 1, 2 => 1}}), Product.search("Product").where(store_id: 2, price: {gt: 5}).aggs(:store_id)
   end
 
   def test_smart_aggs_false
